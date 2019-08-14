d95894c93d0a65b7fe84efdc94cf79b7fe478410
Albertobarrio <albertobarrio2012@gmail.com>
2019-08-14 11:58:48 -0300

    primer commit

---
+++ test_roman_numbers.py
@@ -0,0 +1,15 @@
+import unittest
+
+from ronam_numbers import roman_to_decimal
+
+# Test de numero romano a decimal
+
+
+class TestRomanNumbers(unittest.TestCase):
+    def test_roman_I_to_decimal(self):
+        decimal_number = roman_to_decimal('I')
+        self.assertEqual(decimal_number, 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
